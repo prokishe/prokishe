756f3bbdb1f4920a484c92404d3129839cf967a0
Дмитрий Проканюк <dprokanyuk@astralinux.ru>
2022-10-27 14:49:00 +0300

    task souvenirs

---
+++ 01basics_souvenirs.py
@@ -0,0 +1,6 @@
+souvenirs = int(input('Введите количество сувениров: '))
+trinkets = int(input('Введите количество безделушек: '))
+souvenirs *=75
+trinkets =*112
+wreight = souvenirs + trinkets
+print('Общий вес посылки равен', wreight, 'грамм')
